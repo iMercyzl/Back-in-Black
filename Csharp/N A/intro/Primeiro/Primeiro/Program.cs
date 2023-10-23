71d4784ff49266d8f028c2d7aa354a537a1416fc
Pedro Luiz <pedroluizct3@gmail.com>
2023-10-23 18:07:34 -0300

    iniciando estudos de Csharp

---
+++ Csharp/N A/intro/Primeiro/Primeiro/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso { 
+    class Program {
+        static void Main(string[] args)
+        {
+            int x, y;
+
+            x = 5;
+
+            y = 2 * x;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+        }
+    }
+}
---
+++ Csharp/N A/intro/Segundo/Segundo/Program.cs
@@ -0,0 +1,9 @@
+﻿double b, B, h, area;
+
+b = 6.0;
+B = 8.0;
+h = 5.0;
+
+area = (b + B) / 2.0 * h;
+
+Console.WriteLine(area);
