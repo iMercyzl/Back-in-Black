8621bb767aacc4e9522037bd908f606e150a0946
Pedro Luiz <pedroluizct3@gmail.com>
2023-10-23 22:13:15 -0300

    exercitando loops

---
+++ fcc/ex2.js
@@ -0,0 +1,49 @@
+// let a = 0;
+// let b = 1;
+
+// while (a < 20) {
+//     console.log(a);
+
+//     const temp = a;
+//     a = b;
+//     b = temp + b;
+// }
+
+
+
+
+
+// let n = 0;
+// let resultado = 1;
+
+// while (resultado <=1000) {
+//     n++;
+//     resultado = Math.pow(2,n);
+
+//     console.log(`2^${n} = ${resultado}`);
+
+// }
+
+// console.log(`O menor valor de n para o qual 2^n ultrapassa 1000 é ${n}`);
+
+
+
+// let i = 2;
+
+// while (i <= 10 ) {
+//     console.log(i);
+//     i += 2;
+// }
+
+
+
+let soma = 0;
+let numero = 1;
+
+while (numero <= 10) {
+    soma += numero;
+    numero++;
+}
+
+console.log("A soma dos primeiros 10 números naturais é:", soma);
+
