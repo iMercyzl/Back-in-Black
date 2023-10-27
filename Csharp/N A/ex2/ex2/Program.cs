07d523c680be6930c42018861d225a0a07eb76e9
Pedro Luiz <pedroluizct3@gmail.com>
2023-10-27 15:33:38 -0300

    Csharp

---
+++ Csharp/N A/ex2/ex2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double largura, comprimento, precoMetroQuadrado, area, preco;
+
+            Console.WriteLine("Digite a largura do terreno");
+            largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite o comprimento do terreno");
+            comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite o Preço por metro quadrado");
+            precoMetroQuadrado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = largura * comprimento;
+            preco = area * precoMetroQuadrado;
+
+            Console.WriteLine("area do terreno = " + area.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("preço do terreno = " + preco.ToString("F2", CultureInfo.InvariantCulture));
+
+
+            Console.ReadLine();
+        }
+    }
+}
+
