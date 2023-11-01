376bcb4a2b6497f792589aed15a98e3c73dfd80a
Pedro Luiz <pedroluizct3@gmail.com>
2023-11-01 14:39:24 -0300

    Csharp

---
+++ Csharp/N A/Sexto/Sexto/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, area, perimetro, diagonal;
+
+            a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = a * b;
+            perimetro = 2 * (b + a);
+            diagonal = Math.Sqrt(Math.Pow(a, 2.0) + Math.Pow(b, 2.0));
+
+
+
+            Console.WriteLine("Area = " + area.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perimetro = " + perimetro.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal = " + diagonal.ToString("F4", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
