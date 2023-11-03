319cbacc941fde1a0cb04ddb7fbc2f121890de29
Pedro Luiz <pedroluizct3@gmail.com>
2023-11-02 23:34:03 -0300

    ebac

---
+++ EBAC/medjs/form2/index.js
@@ -0,0 +1,23 @@
+
+const formulario1 = 
+function validaCampoNumero(elemento) {
+    elemento.addEventListener('focusout', function(event){ 
+        event.preventDefault();
+
+        let numero = this.value.math(/^[\d]5-[\d]3/) ? this.value.replace(/-/, '') : this.value;
+
+        if(numero != "" && numero.match(/[0-9]/) && numero >=0 && numero <= 10) {
+            document.querySelector('.mensagem').innerHTML = '';
+            this.classList.remove('erro');
+            this.parentNode.classList.remove('erro');
+
+        }else {
+            document.querySelector(".mensagem").innerHTML = 'verifique o preenchimento dos campos em destaque';
+            this.classList.add('erro');
+            this.parentNode.classList.add('erro');
+            return false;
+        }
+
+
+    });
+}
