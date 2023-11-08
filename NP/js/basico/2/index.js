d8b312d7078e4e3a910a33ef278d55e11a41e1a8
Pedro Luiz <pedroluizct3@gmail.com>
2023-11-08 20:56:25 -0300

    js e csharp

---
+++ NP/js/basico/2/index.js
@@ -0,0 +1,6 @@
+// document.getElementsByClassName('inicial')[0].innerHTML = 'Alterado por JS';
+// document.getElementById('texto').innerHTML = 'meu saco';
+// document.getElementsByTagName('b')[0].innerHTML = 'meu saco 2';
+// document.getElementsByName('nome')[0].value = 'meu saco 3';
+
+document.querySelector('.inicial').innerHTML = 'Alterado por JS';
---
+++ fcc/js.js
@@ -0,0 +1,18 @@
+// let res = 0;
+// let j;
+// for(let i = 0; i <4; i++){
+//     j = 1;
+//     while(j<3) {
+//         res = res +2;
+//         if(res == 0) {continue;}
+//         j = j+1;
+//     }
+// }
+// alert(res);
+
+let nota1 = 5;
+let nota2 ="10";
+let media = nota1+nota2;
+alert(media);
+media = (nota1 + nota2)/2;
+alert(media);
